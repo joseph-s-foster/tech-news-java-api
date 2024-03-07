0a001b535f055cfac5fd424cd62e41e9cc273015
D3C0D1NG <joseph.s.foster@icloud.com>
2024-03-07 17:09:55 -0600

    updating controllers

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -2,90 +2,70 @@ package com.technews.controller;
 
 import com.technews.model.Post;
 import com.technews.model.User;
-import com.technews.model.Vote;
-import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-public class PostController {
+public class UserController {
 
     @Autowired
-    PostRepository repository;
+    UserRepository repository;
 
     @Autowired
     VoteRepository voteRepository;
 
-    @Autowired
-    UserRepository userRepository;
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        for (User u : userList) {
+            List<Post> postList = u.getPosts();
+            for (Post p : postList) {
+                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            }
+        }
+        return userList;
+    }
 
-    @GetMapping("/api/posts")
-    public List<Post> getAllPosts() {
-        List<Post> postList = repository.findAll();
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Integer id) {
+        User returnUser = repository.getById(id);
+        List<Post> postList = returnUser.getPosts();
         for (Post p : postList) {
             p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
         }
-        return postList;
+
+        return returnUser;
     }
 
-
-    @GetMapping("/api/posts/{id}")
-    public Post getPost(@PathVariable Integer id) {
-        Post returnPost = repository.getById(id);
-        returnPost.setVoteCount(voteRepository.countVotesByPostId(returnPost.getId()));
-
-        return returnPost;
+    @PostMapping("/api/users")
+    public User addUser(@RequestBody User user) {
+        // Encrypt password
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+        repository.save(user);
+        return user;
     }
 
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        User tempUser = repository.getById(id);
 
-    @PostMapping("/api/posts")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Post addPost(@RequestBody Post post) {
-        repository.save(post);
-        return post;
-    }
-
-
-    @PutMapping("/api/posts/{id}")
-    public Post updatePost(@PathVariable int id, @RequestBody Post post) {
-        Post tempPost = repository.getById(id);
-        tempPost.setTitle(post.getTitle());
-        return repository.save(tempPost);
-    }
-
-
-    @PutMapping("/api/posts/upvote")
-    public String addVote(@RequestBody Vote vote, HttpServletRequest request) {
-        String returnValue = "";
-
-        if(request.getSession(false) != null) {
-            Post returnPost = null;
-
-            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
-            vote.setUserId(sessionUser.getId());
-            voteRepository.save(vote);
-
-            returnPost = repository.getById(vote.getPostId());
-            returnPost.setVoteCount(voteRepository.countVotesByPostId(vote.getPostId()));
-
-            returnValue = "";
-        } else {
-            returnValue = "login";
+        if (!tempUser.equals(null)) {
+            user.setId(tempUser.getId());
+            repository.save(user);
         }
-
-        return returnValue;
+        return user;
     }
 
-
-    @DeleteMapping("/api/posts/{id}")
+    @DeleteMapping("/api/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@PathVariable int id) {
+    public void deleteUser(@PathVariable int id) {
         repository.deleteById(id);
     }
+
 }
